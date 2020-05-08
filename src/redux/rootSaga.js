5dc6d3cbd8c2a5d7ec7f52791f99d0b28d20e96f
Mirian Okradze <okradzemirian@gmail.com>
2020-05-08 16:04:13 +0400

    added redux-saga and configured

---
+++ src/redux/rootSaga.js
@@ -0,0 +1,5 @@
+import { all, call } from 'redux-saga/effects'
+
+export default function* () {
+    yield all([])
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore } from 'redux-persist'
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './rootReducer'
+import rootSaga from './rootSaga'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewares = [thunk]
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(...middlewares)),
+)
+
+sagaMiddleware.run(rootSaga)
 
 const persistor = persistStore(store)
 
