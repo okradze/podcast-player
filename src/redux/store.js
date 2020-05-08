a8231f3fd286a888c3859a2d9fbfe25005e5c6cd
Mirian Okradze <okradzemirian@gmail.com>
2020-05-08 21:12:44 +0400

    removed redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore } from 'redux-persist'
-import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './rootReducer'
@@ -10,7 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const sagaMiddleware = createSagaMiddleware()
 
-const middlewares = [thunk, sagaMiddleware]
+const middlewares = [sagaMiddleware]
 
 const store = createStore(
     rootReducer,
