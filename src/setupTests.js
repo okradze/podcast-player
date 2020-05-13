352380cc25e0add4ff65cc443f4e092a9082fd01
Mirian Okradze <okradzemirian@gmail.com>
2020-05-13 18:45:34 +0400

    removed enzyme and unused dev dependencies

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,1 @@
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
 import '@testing-library/jest-dom/extend-expect'
-
-configure({ adapter: new Adapter() })
