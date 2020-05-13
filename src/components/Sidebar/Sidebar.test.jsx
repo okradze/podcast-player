c4723f8f8d5db45b966bc7495df0b923328c696d
Mirian Okradze <okradzemirian@gmail.com>
2020-05-13 18:24:14 +0400

    tested Sidebar component

---
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
+import { render, cleanup } from '@testing-library/react'
+import { Sidebar } from './Sidebar'
+
+afterEach(cleanup)
+
+describe('Sidebar', () => {
+    test('render component', () => {
+        const { getByText, getAllByText } = render(
+            <MemoryRouter>
+                <Sidebar />
+            </MemoryRouter>,
+        )
+        getAllByText(/home$/i) // get navlink and not home.svg
+        getByText(/discover/i)
+        getByText(/now playing/i)
+    })
+})
