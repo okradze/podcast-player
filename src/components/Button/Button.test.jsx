115e86ee44dbb684f7f3f7f9324fbbf5a11c9b57
Mirian Okradze <okradzemirian@gmail.com>
2022-01-13 16:37:30 +0400

    refactor: Button and Ellipsis Text testing

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import { Button } from './Button'
 
 afterEach(cleanup)
@@ -7,12 +7,8 @@ afterEach(cleanup)
 describe('Button', () => {
   const text = 'Hello'
 
-  test('renders default version', () => {
-    const { getByText } = render(<Button>{text}</Button>)
-    expect(getByText(text)).toMatchSnapshot()
-  })
-  test('renders inverted version', () => {
-    const { getByText } = render(<Button inverted>{text}</Button>)
-    expect(getByText(text)).toMatchSnapshot()
+  test('renders with text', () => {
+    render(<Button>{text}</Button>)
+    expect(screen.queryByText(text)).toBeInTheDocument()
   })
 })
--- src/components/EllipsisText/EllipsisText.test.jsx
+++ src/components/EllipsisText/EllipsisText.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import { EllipsisText } from './EllipsisText'
 
 afterEach(cleanup)
 
 it('should render EllipsisText component', () => {
   const text = 'Hello'
-  const { getByText } = render(<EllipsisText>{text}</EllipsisText>)
-  expect(getByText(text)).toMatchSnapshot()
+  render(<EllipsisText>{text}</EllipsisText>)
+  expect(screen.queryByText(text)).toBeInTheDocument()
 })
